51357120dfd8da084d747691ae9953eb21fd4066
Justin Huang <justin.2260@gmail.com>
2022-01-18 18:18:55 -0500

    add conditional logic for the styling of the button depending on the type

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,25 @@
 import React from "react";
-
+import classNames from 'classnames';
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  console.log(props);
+  console.log(props.children);
+	console.log(props.disabled);
+
+	const buttonClass = classNames("button", { "button--confirm": props.confirm , "button--danger": props.danger });
+	console.log(buttonClass);
+  // if (props.confirm) { 
+  //   buttonClass += " button--confirm";
+  // }
+
+  // if (props.danger) {
+  //   buttonClass += " button--danger";
+  // }
+
+  return (
+  	<button onClick={props.onClick} className={buttonClass} disabled={props.disabled}>
+			{props.children}
+		</button>
+  );
 }
