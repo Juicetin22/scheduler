614524b29f3d406fd142643e98a997b8d27053eb
Justin Huang <justin.2260@gmail.com>
2022-01-19 00:07:36 -0500

    refactor code to be easier to read

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -6,19 +6,23 @@ import "components/InterviewerList.scss";
 export default function InterviewerList(props) {
   console.log(props);
 
-  const listOfInterviewers = props.interviewers.map(interviewer => {
-    if (interviewer.id === props.interviewer) {
-      return <InterviewerListItem key={interviewer.id} name={interviewer.name} avatar={interviewer.avatar} setInterviewer={() => props.setInterviewer(interviewer.id)} selected />
-    } else {
-      return <InterviewerListItem key={interviewer.id} name={interviewer.name} avatar={interviewer.avatar} setInterviewer={() => props.setInterviewer(interviewer.id)} />
-    }
+  const interviewers = props.interviewers.map(interviewer => {
+    return (
+    <InterviewerListItem 
+      key={interviewer.id} 
+      name={interviewer.name} 
+      avatar={interviewer.avatar} 
+      setInterviewer={() => props.setInterviewer(interviewer.id)} 
+      selected={interviewer.id === props.interviewer} 
+    />
+    );
   });
 
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">
-        {listOfInterviewers}
+        {interviewers}
       </ul>
     </section>
 
