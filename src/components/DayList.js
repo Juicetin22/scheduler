a00d45d1198b18cd841c6ea62f88e485b8df19ab
Justin Huang <justin.2260@gmail.com>
2022-01-19 00:12:44 -0500

    refactor code to read more easily

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,15 +3,19 @@ import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
 
-  const listOfDays = props.days.map(day => {
-    if (day.name === props.day) {
-      return <DayListItem key={day.id} name={day.name} spots={day.spots} setDay={props.setDay} selected /> 
-    } else {
-      return <DayListItem key={day.id} name={day.name} spots={day.spots} setDay={props.setDay} /> 
-    }
-  })
+  const days = props.days.map(day => {
+    return (
+      <DayListItem 
+        key={day.id} 
+        name={day.name} 
+        spots={day.spots} 
+        setDay={props.setDay} 
+        selected={day.name === props.day} 
+      />
+    ); 
+  });
   
   return (
-    <ul>{listOfDays}</ul>
+    <ul>{days}</ul>
   );
 }
