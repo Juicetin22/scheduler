6a94d5554cceb66fbd9e9f6fb06dfbaaaf4adfa6
Justin Huang <justin.2260@gmail.com>
2022-01-19 21:12:23 -0500

    update Appointment component to have a different display depending on whether there are any appointments or not

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,17 +2,24 @@ import React from "react";
 
 import "./styles.scss";
 
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 export default function Appointment(props) {
   
-  const appointment = (time) => {
-    if (!time) {
-      return "No Appointments"
-    } else {
-      return `Appointment at ${time}`
-    }
-  };
+  // const appointment = (time) => {
+  //   if (!time) {
+  //     return "No Appointments"
+  //   } else {
+  //     return `Appointment at ${time}`
+  //   }
+  // };
 
   return (
-    <article className="appointment">{appointment(props.time)}</article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+    </article>
   );
 } 
