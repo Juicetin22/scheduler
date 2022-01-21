e8b87dfed5c2fa6f634bdca1253f1345319d5c0a
Justin Huang <justin.2260@gmail.com>
2022-01-20 19:03:41 -0500

    create helper function getAppointmentsForDay and test with appropriate unit tests

---
+++ src/helpers/selectors.js
@@ -0,0 +1,18 @@
+export function getAppointmentsForDay(state, day) {
+  const selectedDay = state.days.filter(oneDay => oneDay.name === day);
+  
+  // if filtered array contains no data, return the empty array
+  if (selectedDay.length < 1) {
+    return selectedDay;
+  }
+
+  const listOfAppointments = selectedDay[0].appointments.map(id => {
+    for (const appointment in state.appointments) {
+      if (id === state.appointments[appointment].id) {
+        return state.appointments[appointment];
+      }
+    }
+  });
+
+  return listOfAppointments;
+}
