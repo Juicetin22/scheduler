4619ba32a5b970aa9a54e3f07b259dc3edb6ab85
Justin Huang <justin.2260@gmail.com>
2022-01-21 21:35:18 -0500

    implement the custom hook changing the different modes in the Appointment component; implement for EMPTY, SHOW, and CREATE modes

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,9 +5,19 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
   
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+    );
   // const appointment = (time) => {
   //   if (!time) {
   //     return "No Appointments"
@@ -19,7 +29,14 @@ export default function Appointment(props) {
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && <Form interviewers={[]} onCancel={() => back()} />}
     </article>
   );
 } 
