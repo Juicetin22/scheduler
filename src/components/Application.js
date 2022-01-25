55d0e3cdcbe45e111baf742b0ef709b73e58222b
Justin Huang <justin.2260@gmail.com>
2022-01-24 21:27:01 -0500

    move logic and data retrieving out of application component to a hook file

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,141 +1,29 @@
-import React, { useEffect, useState } from "react";
-import axios from 'axios';
+import React from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
-
-// const appointments = [
-//   {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer:{
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer:{
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   {
-//     id: 5,
-//     time: "4pm",
-//   }
-// ];
-
+import useApplicationData from "hooks/useApplicationData";
 
 export default function Application(props) {
-  // const [day, setDay] = useState("Monday");
-  // const [days, setDays] = useState([]);
-
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  })
-
-  const setDay = day => setState({ ...state, day });
-  // const setDays = days => setState(prev => ({ ...prev, days }));
-
-  // useEffect(() => {
-  //   axios.get('/api/days')
-  //     .then((response) => {
-  //       console.log(response.data);
-  //       setDays(response.data);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error.response.status);
-  //       console.log(error.response.headers);
-  //       console.log(error.response.data);
-  //     });
-  // }, []);
-
-  useEffect(() => {
-    Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
-    ]).then((all) => {
-      console.log(all[0]); // first
-      console.log(all[1]); // second
-      console.log(all[2]);
-    
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
-    }).catch(error => console.log(`Error: ${error.message}`));
-  }, []);
+  
+  const { state, setDay, bookInterview, cancelInterview } = useApplicationData();
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   const interviewersForDay = getInterviewersForDay(state, state.day);
 
-  function bookInterview(id, interview) {
-    console.log(id, interview);
-
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    
-    return axios.put(`/api/appointments/${id}`, { interview })
-      .then(() => {
-        setState({
-          ...state,
-          appointments
-        });
-      });
-  }   
-  
-  function cancelInterview(id) {
-    const cancelledAppointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-
-    const updatedAppointments = {
-      ...state.appointments,
-      [id]: cancelledAppointment
-    };
-    
-    return axios.delete(`/api/appointments/${id}`)
-      .then(() => {
-        setState({
-          ...state,
-          updatedAppointments
-        })
-      });
-  }
-  
   const appointmentList = dailyAppointments.map(appointment => {
     const interview = getInterview(state, appointment.interview);
 
     return (
-      <Appointment key={appointment.id} {...appointment} interview={interview} interviewers={interviewersForDay} bookInterview={bookInterview} cancelInterview={cancelInterview} />
+      <Appointment 
+        key={appointment.id} 
+        {...appointment} 
+        interview={interview} 
+        interviewers={interviewersForDay} 
+        bookInterview={bookInterview} 
+        cancelInterview={cancelInterview} 
+      />
     );
   });
 
