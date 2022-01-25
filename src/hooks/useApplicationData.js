f119d61a5a280879624879cc092c1565711dfa1c
Justin Huang <justin.2260@gmail.com>
2022-01-24 21:27:59 -0500

    place logic and data retrieval to this file to separate logic from displaying code

---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,71 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function useApplicationData() {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  })
+
+  const setDay = day => setState({ ...state, day });
+
+  useEffect(() => {
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
+    ]).then((all) => {
+      console.log(all[0]); // first
+      console.log(all[1]); // second
+      console.log(all[2]);
+    
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
+    }).catch(error => console.log(`Error: ${error.message}`));
+  }, []);
+
+  function bookInterview(id, interview) {
+    console.log(id, interview);
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    
+    return axios.put(`/api/appointments/${id}`, { interview })
+      .then(() => {
+        setState({
+          ...state,
+          appointments
+        });
+      });
+  }   
+  
+  function cancelInterview(id) {
+    const cancelledAppointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+
+    const updatedAppointments = {
+      ...state.appointments,
+      [id]: cancelledAppointment
+    };
+    
+    return axios.delete(`/api/appointments/${id}`)
+      .then(() => {
+        setState({
+          ...state,
+          updatedAppointments
+        })
+      });
+  }
+
+  return { state, setDay, bookInterview, cancelInterview };
+}
