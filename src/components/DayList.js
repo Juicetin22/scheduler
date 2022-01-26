fd3c671eeb8b94b7e83cc5d5becaea3d2625688a
Justin Huang <justin.2260@gmail.com>
2022-01-26 00:09:47 -0500

    add comments

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,7 +2,7 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-
+  // retrieve an array of days using the props sent from Application
   const days = props.days.map(day => {
     return (
       <DayListItem 
