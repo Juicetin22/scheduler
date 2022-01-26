9dd898adb1bd572fbf0ca2068e3a734c4794e385
Justin Huang <justin.2260@gmail.com>
2022-01-25 23:00:52 -0500

    clean code and remove console logs

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,19 +3,7 @@ import classNames from 'classnames';
 import "components/Button.scss";
 
 export default function Button(props) {
-  console.log(props);
-  console.log(props.children);
-	console.log(props.disabled);
-
-	const buttonClass = classNames("button", { "button--confirm": props.confirm , "button--danger": props.danger });
-	console.log(buttonClass);
-  // if (props.confirm) { 
-  //   buttonClass += " button--confirm";
-  // }
-
-  // if (props.danger) {
-  //   buttonClass += " button--danger";
-  // }
+  const buttonClass = classNames("button", { "button--confirm": props.confirm , "button--danger": props.danger });
 
   return (
   	<button onClick={props.onClick} className={buttonClass} disabled={props.disabled}>
