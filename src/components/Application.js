904c13c9edac3caeccd1f371dbfa9d0a6000b612
Justin Huang <justin.2260@gmail.com>
2022-01-26 00:02:51 -0500

    add comments

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,12 +6,15 @@ import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "help
 import useApplicationData from "hooks/useApplicationData";
 
 export default function Application(props) {
-  
+  // retrieve from hook to use in the component
   const { state, setDay, bookInterview, cancelInterview } = useApplicationData();
 
+  // retrieve the appointments for a particular day using the current state and day
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  // retrieve the interviewers that are available for a particular day
   const interviewersForDay = getInterviewersForDay(state, state.day);
 
+  // create the array of Appointment components for the day - each component receieves info from an individual appointment as props
   const appointmentList = dailyAppointments.map(appointment => {
     const interview = getInterview(state, appointment.interview);
 
