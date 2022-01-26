4db2c0ee1650e5cb1320c54a0dec1da8679245f5
Justin Huang <justin.2260@gmail.com>
2022-01-26 00:04:37 -0500

    add comments

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,6 +3,7 @@ import classNames from 'classnames';
 import "components/Button.scss";
 
 export default function Button(props) {
+  // add different button classes to the button component depending on type of button
   const buttonClass = classNames("button", { "button--confirm": props.confirm , "button--danger": props.danger });
 
   return (
