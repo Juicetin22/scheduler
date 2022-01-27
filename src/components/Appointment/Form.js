247252a873f51a207cee51fabe48b8e80e346e9e
Justin Huang <justin.2260@gmail.com>
2022-01-26 23:20:39 -0500

    incorporate preventing of form submission if an interviewer is not selected

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
@@ -7,12 +7,14 @@ export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
   const [error, setError] = useState("");
+  const [interviewerError, setInterviewerError] = useState("");
 
   // function that is called within cancel function to reset states
   const reset = () => {
     setStudent("");
     setInterviewer(null);
     setError("");
+    setInterviewerError("");
   };
 
   // function that calls function from props (eventually calls back()) that brings mode back to previous
@@ -23,16 +25,36 @@ export default function Form(props) {
   
   // function that gets called when saving the form
   const validate = () => {
+    
+    // prevents form from being submitted if both student input field is empty and interviewer is not selected
+    if (student === "" && interviewer === null) {
+      setError("Student name cannot be blank");
+      setInterviewerError("An interviewer must be selected");
+      return;
+    }
+    
     // prevents form from being submitted if student input field is empty
     if (student === "") {
       setError("Student name cannot be blank");
       return;
     }
 
-    // return error state to initial and call props.onSave function with the form data
+    // prevents form from being submitted if interviewer is not selected
+    if (interviewer === null) {
+      setInterviewerError("An interviewer must be selected");
+      return;
+    }
+
+    // if the checks pass, return error state to initial and call props.onSave function with the form data
     setError("");
+    setInterviewerError("");
     props.onSave(student, interviewer)
   };
+
+  // remove interviewer error message after appearing once an interviewer is selected
+  useEffect(() => {
+    setInterviewerError("");
+  }, [interviewer]);
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -52,6 +74,7 @@ export default function Form(props) {
           />
         </form>
         <section className="appointment__validation">{error}</section>
+        <section className="appointment__validation">{interviewerError}</section>
         <InterviewerList 
           interviewers={props.interviewers}
           value={interviewer}
